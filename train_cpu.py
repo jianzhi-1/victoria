99ee97258df64ea86b92956c8bb160e2303451a0
jianzhi-1 <jianzhiw00@gmail.com>
2026-06-28 01:06:59 -0400

    eval within train

--- train_cpu.py
+++ train_cpu.py
@@ -7,7 +7,8 @@ from torch.utils.data import DataLoader
 from streaming_dataset import StreamingDataset, get_splits
 from basic.basic_net import BasicNet
 
-def train(B: int):
+
+def train(B: int, epochs_per_checkpoint: int, epochs_per_eval: int):
     torch.manual_seed(42)
     dataset = StreamingDataset(path_prefix="./data/data")
     ratios = {
@@ -27,12 +28,12 @@ def train(B: int):
         print(X.shape, y.shape)
 
     D = 16
-    NUM_EPOCHS = 10
+    NUM_EPOCHS = 100
     net = BasicNet(D, 1)
-    net.train()
     loss_fn = nn.MSELoss(reduction="sum")
     optimizer = torch.optim.Adam(net.parameters())
     for epoch in range(NUM_EPOCHS):
+        net.train()
         total_loss = 0.
         total_n = 0
         for X, y in train_dataloader:
@@ -43,24 +44,28 @@ def train(B: int):
             total_n += X.shape[0]
             loss.backward()
             optimizer.step()
-        print(f"epoch {epoch} done; avg loss {total_loss / total_n}")
+        print(f"[train] {epoch}/{NUM_EPOCHS}: avg loss {total_loss / total_n}")
 
-    # Eval
-    net.eval()
-    with torch.no_grad():
-        total_loss = 0.
-        total_n = 0
-        for X, y in eval_dataloader:
-            yhat = net(X)
-            loss = loss_fn(yhat, y)
-            total_loss += loss.item()
-            total_n += X.shape[0]
-        print(f"validation done; avg loss {total_loss / total_n}")
-    
-    torch.save(net.state_dict(), "./models/basic.pth")
+        if (epoch + 1) % epochs_per_eval == 0 or (epoch + 1) == NUM_EPOCHS:
+            # Eval
+            net.eval()
+            with torch.no_grad():
+                total_loss = 0.
+                total_n = 0
+                for X, y in eval_dataloader:
+                    yhat = net(X)
+                    loss = loss_fn(yhat, y)
+                    total_loss += loss.item()
+                    total_n += X.shape[0]
+                print(f"[eval] {epoch}/{NUM_EPOCHS}: avg loss {total_loss / total_n}")
+        
+        if (epoch + 1) % epochs_per_checkpoint == 0:
+            torch.save(net.state_dict(), f"./models/checkpoint_{epoch}_{NUM_EPOCHS}_basic.pth")
+
+    torch.save(net.state_dict(), f"./models/checkpoint_{NUM_EPOCHS}_{NUM_EPOCHS}_basic.pth")
 
 if __name__ == "__main__":
-    train(16)
+    train(16, 50, 10)
 
 
 
