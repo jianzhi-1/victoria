4973bf4f9d46be43d2259b2e75c44504e8d16acc
jianzhi-1 <jianzhiw00@gmail.com>
2026-06-29 16:38:40 -0400

    pivot

--- custom/gdn/gdn.py
+++ custom/gdn/gdn.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
 class GDN(nn.Module):
     def __init__(self, D: int, DV: int, DK: int) -> None:
@@ -13,16 +14,20 @@ class GDN(nn.Module):
         self.Wk = nn.Linear(D, DK)
         self.Wv = nn.Linear(D, DV)
 
-    def forward(self, x: torch.Tensor, prev_S: torch.Tensor) -> tuple[torch.Tensor, torch.Tensor]:
+    def forward(self, x: torch.Tensor, prev_S: torch.Tensor, mask: torch.Tensor | None = None) -> tuple[torch.Tensor, torch.Tensor]:
         B, _ = x.shape
         assert x.shape == (B, self.D), [x.shape, (B, self.D)]
         assert prev_S.shape == (B, self.DV, self.DK), [prev_S.shape, (B, self.DV, self.DK)]
+        if mask is None:
+            mask = x.new_ones(size=(B, 1))
+        assert mask.shape == (B, 1), [mask.shape, (B, 1)]
+        mask = mask.view(B, 1, 1)
         Q, K, V = self.Wq(x), self.Wk(x), self.Wv(x)
 
         alpha = torch.sigmoid(self.Wa(x)).view(B, 1, 1)
         beta = torch.sigmoid(self.Wb(x)).view(B, 1, 1)
-        S = alpha * prev_S - alpha * beta * torch.einsum("bv,bk->bvk", torch.einsum("bvk,bk->bv", prev_S, K), K) + beta * torch.einsum("bv,bk->bvk", V, K)
-        out = torch.einsum("bvk,bk->bv", S, Q)
+        S = prev_S - (1. - alpha) * mask * prev_S - alpha * beta * mask * torch.einsum("bv,bk->bvk", torch.einsum("bvk,bk->bv", prev_S, K), K) + beta * mask * torch.einsum("bv,bk->bvk", V, K)
+        out = torch.einsum("bvk,bk->bv", S * mask, Q)
         return out, S
 
 if __name__ == "__main__":
@@ -33,9 +38,13 @@ if __name__ == "__main__":
     DK = 16
     gdn = GDN(D, DV, DK)
     S = torch.zeros(B, DV, DK, requires_grad=False)
-    L = 10
-    for _ in range(L):
+    MIN_SEQ_LEN = 5
+    MAX_SEQ_LEN = 512
+    L = MAX_SEQ_LEN
+    SEQ_LEN = F.one_hot(torch.randint(MIN_SEQ_LEN, MAX_SEQ_LEN, (B, 1)), num_classes=MAX_SEQ_LEN)
+    mask = torch.arange(MAX_SEQ_LEN)[None, :] < torch.randint(MIN_SEQ_LEN, MAX_SEQ_LEN, (B, 1))
+    for i in range(L):
         x = torch.randn(size=(B, D))
-        out, S = gdn(x, S)
+        out, S = gdn(x, S, mask[:,i:(i+1)])
         print(out)
     print("DONE")
